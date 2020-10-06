8e2dfa73dd153e0426a6b28ddaac8f4fabed6de8
ksm0207 <ksm03071@naver.com>
2020-10-06 15:41:07 +0900

    React-Native 날씨앱 만들기
    - 프로젝트 세팅확인
    - Expo 앱 구동확인

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>2020-10-06 React Native 날씨앱 만들기 !!</Text>
       <StatusBar style="auto" />
     </View>
   );
