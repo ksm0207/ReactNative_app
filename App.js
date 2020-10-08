d573887cbcc4f8008a633496472db5272bc0bd7e
ksm0207 <ksm03071@naver.com>
2020-10-09 04:09:45 +0900

    구문수정

--- App.js
+++ App.js
@@ -6,16 +6,15 @@ import { Alert } from "react-native";
 export default class extends React.Component {
   getLocation = async () => {
     state = {
-      isLoading: true
+      isLoading: true,
     };
     try {
       await Location.requestPermissionsAsync(); // 퍼미션요청
       const {
-        coords: { latitude, longitude }
+        coords: { latitude, longitude },
         // latitude , longitude를 가져왔을때 잠시 setState를 진행
       } = await Location.getCurrentPositionAsync();
       this.setState({ isLoading: false });
-      // Send to API and get Weather !!
     } catch (error) {
       Alert.alert("False");
     }
@@ -27,6 +26,6 @@ export default class extends React.Component {
   render() {
     const { isLoading } = this.state;
     // 만약 isLoading이 있으면 <Loading>/ 을 리턴하고 아니면 null을 리턴
-    return isLoading ? <Loading></Loading> : null;
+    return isLoading ? <Loading /> : null;
   }
 }
