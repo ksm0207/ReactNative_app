667ea1139ec430cbf688b05e6fe859be91e74c97
ksm0207 <ksm03071@naver.com>
2020-10-09 04:04:51 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
 
-export default class extends React.Component{
-  
-  getLocation = async() =>{
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+export default class extends React.Component {
+  getLocation = async () => {
+    state = {
+      isLoading: true
+    };
+    try {
+      await Location.requestPermissionsAsync(); // 퍼미션요청
+      const {
+        coords: { latitude, longitude }
+        // latitude , longitude를 가져왔을때 잠시 setState를 진행
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+      // Send to API and get Weather !!
+    } catch (error) {
+      Alert.alert("False");
+    }
   };
-
-  componentDidMount(){
+  componentDidMount() {
     this.getLocation();
   }
-  
-  render(){
-    return <Loading></Loading>
+
+  render() {
+    const { isLoading } = this.state;
+    // 만약 isLoading이 있으면 <Loading>/ 을 리턴하고 아니면 null을 리턴
+    return isLoading ? <Loading></Loading> : null;
   }
 }
