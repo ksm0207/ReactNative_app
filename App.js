b66e0a189962bac70ed06a0cabc4acc2f68f830d
ksm0207 <ksm03071@naver.com>
2020-10-12 17:17:44 +0900

    구문 오류수정
    - state 구문수정 하였습니다

--- App.js
+++ App.js
@@ -4,17 +4,16 @@ import * as Location from "expo-location";
 import { Alert } from "react-native";
 
 export default class extends React.Component {
+  state ={
+    isLoading: true
+  }
   getLocation = async () => {
-    state = {
-      isLoading: true,
-    };
     try {
       await Location.requestPermissionsAsync(); // 퍼미션요청
-      const {
-        coords: { latitude, longitude },
-        // latitude , longitude를 가져왔을때 잠시 setState를 진행
-      } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: false });
+      const {coords:{latitude,longitude}
+    } =  await Location.getCurrentPositionAsync();
+    this.setState({isLoading:false});
+      // API로 전송해서 날씨가져오기
     } catch (error) {
       Alert.alert("False");
     }
@@ -22,10 +21,10 @@ export default class extends React.Component {
   componentDidMount() {
     this.getLocation();
   }
-
   render() {
-    const { isLoading } = this.state;
+    const { isLoading } = this.state
     // 만약 isLoading이 있으면 <Loading>/ 을 리턴하고 아니면 null을 리턴
-    return isLoading ? <Loading /> : null;
+    return isLoading ? <Loading></Loading> : null;
   }
+  
 }
