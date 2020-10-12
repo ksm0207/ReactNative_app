f2e6446053fcb56d5bc9641d8d69b3bf9d381e22
ksm0207 <ksm03071@naver.com>
2020-10-12 18:03:01 +0900

    APIKEY 호출완료
    - axios 설치
    - API호출확인

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import axios from "axios";
+
+
+const API_KEY = "d31ea6055ce5d867a87f021705cca0ab";
 
 export default class extends React.Component {
   state ={
     isLoading: true
+  }
+  getWeather = async(latitude,longitude)=>{
+    const {data} = await axios.get(
+      // URL
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   }
   getLocation = async () => {
     try {
@@ -13,6 +24,7 @@ export default class extends React.Component {
       const {coords:{latitude,longitude}
     } =  await Location.getCurrentPositionAsync();
     this.setState({isLoading:false});
+    this.getWeather(latitude,longitude);
       // API로 전송해서 날씨가져오기
     } catch (error) {
       Alert.alert("False");
